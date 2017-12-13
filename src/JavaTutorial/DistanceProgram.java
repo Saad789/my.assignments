7483bbe6c221d90be4d9fabfc6084a8e98f1d263
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2017-12-13 11:41:44 +0530

    Add files via upload

--- src/JavaTutorial/DistanceProgram.java
+++ src/JavaTutorial/DistanceProgram.java
@@ -3,20 +3,23 @@ package JavaTutorial;
 public class DistanceProgram {
 
 	public static void main(String[] args) {
-		int distanceinmeters = 20000;
-		double km = distanceinmeters/1000.0;
-		double mile = distanceinmeters/1609.0;
-		int timeinhours = 2;
-		int timeinminutes = 30;
-		int timeinseconds = 20;
-		double totaltimeinhours = timeinhours + (timeinminutes/60.0) + (timeinseconds/3600.0);
-		double totaltimeinsec = (2*3600) + (30*60) + 20;
-		double speedinmts = distanceinmeters/totaltimeinsec;
-		double speedinkms = km/timeinhours;
-		double speedinmiles = mile/totaltimeinhours;
-		System.out.println("speedinmts= "+speedinmts);
-		System.out.println("speedinkms= "+speedinkms);
-		System.out.println("speedinmiles= "+speedinmiles);	
+		int distanceInMeters = 20000;
+		double km = distanceInMeters/1000.0;
+		double mile = distanceInMeters/1609.0;
+		
+		int timeInHours = 2;
+		int timeInMinutes = 30;
+		int timeInSeconds = 20;
+		
+		double totalTimeInHours = timeInHours + (timeInMinutes/60.0) + (timeInSeconds/3600.0);
+		double totalTimeInSec = (2*3600) + (30*60) + 20;
+		double speedInMts = distanceInMeters/totalTimeInSec;
+		double speedInKms = km/timeInHours;
+		double speedInMiles = mile/totalTimeInHours;
+		
+		System.out.println("speedInMts= "+speedInMts);
+		System.out.println("speedInKms= "+speedInKms);
+		System.out.println("speedInMiles= "+speedInMiles);	
 				
 		
 
